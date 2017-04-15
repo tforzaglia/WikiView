b7a62ecdc7a04a5bb0a77fb5668af34d2c5e446e
Tom Forzaglia <thomas.forzaglia@gmail.com>
2017-04-15 16:33:03 -0400

    initial commit with xcodeproj

---
+++ WikiView/WikiView.h
@@ -0,0 +1,19 @@
+//
+//  WikiView.h
+//  WikiView
+//
+//  Created by Thomas Forzaglia on 4/15/17.
+//  Copyright © 2017 Thomas Forzaglia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WikiView.
+FOUNDATION_EXPORT double WikiViewVersionNumber;
+
+//! Project version string for WikiView.
+FOUNDATION_EXPORT const unsigned char WikiViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WikiView/PublicHeader.h>
+
+
