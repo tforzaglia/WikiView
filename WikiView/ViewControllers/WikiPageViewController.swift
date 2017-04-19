2c081905678899e437ab44b6166bf185951b0489
Tom Forzaglia <thomas.forzaglia@gmail.com>
2017-04-18 21:27:42 -0400

    add url image view

---
+++ WikiView/ViewControllers/WikiPageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  WikiPageViewController.swift
+//  WikiView
+//
+//  Created by Thomas Forzaglia on 4/16/17.
+//  Copyright © 2017 Thomas Forzaglia. All rights reserved.
+//
+
+import UIKit
+
+class WikiPageViewController: UIViewController {
+    private let titleLabel = UILabel(frame: .zero)
+    private let bioTextView = UITextView(frame: .zero)
+}
